7063c9526604b7916b6d6488a86c3067753db782
Raymond Deng <1107450@gmail.com>
2024-02-26 20:04:31 -0800

    masked self attention

--- lib/ViT/na_vit.ml
+++ lib/ViT/na_vit.ml
@@ -57,6 +57,7 @@ module NaViT = struct
     let arange = Torch.arange1 ~device in
     let pad_sequence = Torch.nn.functional.pad_sequence ~batch_first:true in
 
+    (* Auto pack if specified *)
     let batched_images =
       if group_images then
         group_images_by_max_seq_len
@@ -66,3 +67,54 @@ module NaViT = struct
           ~max_seq_len:group_max_seq_len
       else
         input_images
+    in
+
+    (* Process images into variable length sequences with attention mask *)
+    let num_images, batched_sequences, batched_positions, batched_image_ids =
+      List.fold_left
+        ~f:(fun (num_images_acc, seq_acc, pos_acc, ids_acc) images ->
+            let num_images = num_images_acc + 1 in
+            let sequences, positions, image_ids =
+              List.fold_left
+                ~f:(fun (seq_acc, pos_acc, ids_acc) (image_id, image) ->
+                    let image_dims = Tensor.shape image in
+                    let ph, pw = Tensor.map ~f:(fun d -> Int.div d p) image_dims in
+
+                    let pos =
+                      Tensor.meshgrid [arange ph; arange pw]
+                      |> Tensor.stack ~dim:2
+                      |> Tensor.view ~size:[-1; 2]
+                    in
+
+                    let seq =
+                      Tensor.view image ~size:[c; ph; p; pw]
+                      |> Tensor.permute ~dims:[1; 3; 0; 2]
+                      |> Tensor.view ~size:[-1; c * p * p]
+                    in
+
+                    let seq_len = Tensor.size seq |> List.last_exn in
+
+                    let seq, pos =
+                      match t.model_config.calc_token_dropout with
+                      | Some token_dropout ->
+                        let token_dropout_prob = token_dropout image_dims in
+                        let num_keep = max 1 (Int.of_float (Float.of_int seq_len * (1. -. token_dropout_prob))) in
+                        let keep_indices = Tensor.normal_ ~std:1. |> Tensor.topk ~k:num_keep ~dim:0 in
+
+                        Tensor.index_select seq ~dim:0 ~index:keep_indices,
+                        Tensor.index_select pos ~dim:0 ~index:keep_indices
+                      | None -> seq, pos
+                    in
+
+                    Tensor.cat seq_acc seq ~dim:0,
+                    Tensor.cat pos_acc pos ~dim:0,
+                    Tensor.cat ids_acc (Tensor.full_like seq (Scalar.of_int image_id)) ~dim:0
+                )
+                ~init:(Tensor.empty [], Tensor.empty [], Tensor.empty [])
+                images
+            in
+
+            num_images, Tensor.cat seq_acc ~dim:0, Tensor.cat pos_acc ~dim:0, Tensor.cat ids_acc ~dim:0
+        )
+        ~init:(0, Tensor.empty [], Tensor.empty [], Tensor.empty [])
+        batched_images
