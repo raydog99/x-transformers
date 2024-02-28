ca2500cb81843806f2e22facd8720b623cf6bd5e
Raymond Deng <1107450@gmail.com>
2024-02-27 21:15:19 -0800

    init twins vit test

---
+++ test/test_twins_vit.ml
@@ -0,0 +1,29 @@
+open Base
+open Torch
+open TwinsSVT
+
+(* Dataset: non-small cell lung cancer histopathology, ISICDM '21 *)
+let evaluate_twins_model ~image_set_basename ~nbr_of_classes ~labels_names =
+  let data_dir = "/3.testing/img" in
+  let input_path = Filename.concat data_dir (image_set_basename ^ "*") in
+  let data_files = Sys.readdir input_path in
+
+  let count_slides = ref 0 in
+
+  let label_names =
+    In_channel.read_lines labels_names
+    |> List.filter ~f:(fun line -> not (String.is_empty line))
+  in
+
+  for i = 0 to Array.length data_files - 1 do
+    let next_slide = data_files.(i) in
+    printf "New Slide ------------ %d\n" !count_slides;
+
+    let label_index = int_of_string (List.last_exn (String.split ~on:'_' next_slide) |> String.split ~on:'.' |> List.hd_exn) in
+    let label_name = List.nth_exn label_names label_index in
+
+    printf "label %d: %s\n" label_index label_name;
+  done
+
+let () =
+  let image_set_basename = "test_"
